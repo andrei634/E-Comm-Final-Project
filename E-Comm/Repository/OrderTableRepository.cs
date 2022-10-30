48c2d9d594bab196b5251a03d5db09002dcf0d5f
Andrei <andrei.25479@gmail.com>
2022-10-30 20:58:33 +0200

    Added the logic in Repositories for every Table. Created the Controllers for every Table.

--- E-Comm/Repository/OrderTableRepository.cs
+++ E-Comm/Repository/OrderTableRepository.cs
@@ -1,6 +1,104 @@
-﻿namespace E_Comm.Repository
+﻿using E_Comm.Models;
+using E_Comm.Models.DBObjects;
+
+namespace E_Comm.Repository
 {
     public class OrderTableRepository
     {
+        private readonly EcommContext _DbContext;
+
+        public OrderTableRepository()
+        {
+            _DbContext = new EcommContext();
+        }
+
+        public OrderTableRepository(EcommContext dbContext)
+        {
+            _DbContext = dbContext;
+        }
+
+        private OrderTableModel MapDBObjectToModel(OrderTable dbObject)
+        {
+            var model = new OrderTableModel();
+
+            if (dbObject != null)
+            {
+                model.IdCart = dbObject.IdCart;
+                model.Date = dbObject.Date;
+                model.TotalPrice = dbObject.TotalPrice;
+                model.City = dbObject.City;
+                model.ZipCode = dbObject.ZipCode;
+                model.Street = dbObject.Street;
+                model.StreetNumber = dbObject.StreetNumber;
+                model.IdUser = dbObject.IdUser;
+            }
+
+            return model;
+        }
+
+        private OrderTable MapModelToDBObject(OrderTableModel model)
+        {
+            var dbObject = new OrderTable();
+
+            if (model != null)
+            {
+                dbObject.IdCart = model.IdCart;
+                dbObject.Date = model.Date;
+                dbObject.TotalPrice = model.TotalPrice;
+                dbObject.City = model.City;
+                dbObject.ZipCode = model.ZipCode;
+                dbObject.Street = model.Street;
+                dbObject.StreetNumber = model.StreetNumber;
+                dbObject.IdUser = model.IdUser;
+            }
+
+            return dbObject;
+        }
+
+        public List<OrderTableModel> GetAllOrders()
+        {
+            var list = new List<OrderTableModel>();
+
+            foreach (var dbObject in _DbContext.OrderTables)
+            {
+                list.Add(MapDBObjectToModel(dbObject));
+            }
+
+            return list;
+        }
+
+        public OrderTableModel GetOrderById(Guid id)
+        {
+            return MapDBObjectToModel(_DbContext.OrderTables.FirstOrDefault(x => x.IdCart == id));
+        }
+
+        public void InsertOrder(OrderTableModel model)
+        {
+            model.IdCart = Guid.NewGuid();
+            _DbContext.OrderTables.Add(MapModelToDBObject(model));
+            _DbContext.SaveChanges();
+        }
+
+        public void UpdateOrder(OrderTableModel model)
+        {
+            var dbObject = _DbContext.OrderTables.FirstOrDefault(x => x.IdCart == model.IdCart);
+
+            if (dbObject != null)
+            {
+                dbObject.IdCart = model.IdCart;
+                _DbContext.SaveChanges();
+            }
+        }
+
+        public void DeleteOrder(Guid id)
+        {
+            var dbObject = _DbContext.OrderTables.FirstOrDefault(x => x.IdCart == id);
+
+            if (dbObject != null)
+            {
+                _DbContext.OrderTables.Remove(dbObject);
+                _DbContext.SaveChanges();
+            }
+        }
     }
 }
--- E-Comm/Repository/ProductCartRepository.cs
+++ E-Comm/Repository/ProductCartRepository.cs
@@ -1,6 +1,60 @@
-﻿namespace E_Comm.Repository
+﻿using E_Comm.Models;
+using E_Comm.Models.DBObjects;
+
+namespace E_Comm.Repository
 {
     public class ProductCartRepository
     {
+        private readonly EcommContext _DbContext;
+
+        public ProductCartRepository()
+        {
+            _DbContext = new EcommContext();
+        }
+
+        public ProductCartRepository(EcommContext dbContext)
+        {
+            _DbContext = dbContext;
+        }
+
+        private ProductCartModel MapDBObjectToModel(ProductCart dbObject)
+        {
+            var model = new ProductCartModel();
+
+            if(dbObject != null)
+            {
+                model.IdProduct = dbObject.IdProduct;
+                model.IdCart = dbObject.IdCart;
+                model.Quantity = dbObject.Quantity;
+            }
+
+            return model;
+        }
+
+        private ProductCart MapModelToDBObject(ProductCartModel model)
+        {
+            var dbObject = new ProductCart();
+
+            if(model != null)
+            {
+                dbObject.IdProduct = model.IdProduct;
+                dbObject.IdCart = model.IdCart;
+                dbObject.Quantity = model.Quantity;
+            }
+
+            return dbObject;
+        }
+
+        public List<ProductCartModel> GetAllProductCart()
+        {
+            var list = new List<ProductCartModel>(); 
+            foreach(var dbObject in _DbContext.ProductCarts)
+            {
+                list.Add(MapDBObjectToModel(dbObject));
+            }
+
+            return list;
+        }
+
     }
 }
