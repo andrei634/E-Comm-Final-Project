9c17faf8ac72ce9dd2eb2a420d4e9b71cecb181e
andrei634 <84793697+andrei634@users.noreply.github.com>
2022-11-03 19:40:30 +0200

    Delete E-Comm directory

--- E-Comm/Models/CartModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace E_Comm.Models
-{
-    public class CartModel
-    {
-        public Guid IdCart { get; set; }
-    }
-}
--- E-Comm/Models/ProductCartModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace E_Comm.Models
-{
-    public class ProductCartModel
-    {
-        public Guid IdProduct { get; set; }
-        public Guid IdCart { get; set; }
-        public int Quantity { get; set; }
-    }
-}
